e5e38cbf4d6825738c503231c0f4bcfc1fefcae2
Naziha <aankour.naziha@gmail.com>
2025-04-09 18:38:58 +0200

    structures ok

--- srcs/parsing/test.c
+++ srcs/parsing/test.c
@@ -1,1 +1,16 @@
-fghrghtf
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: naankour <naankour@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 15:31:52 by naankour          #+#    #+#             */
+/*   Updated: 2025/04/09 15:31:52 by naankour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// creer des tokens
+// separer ligne de commande chaque fois quil y a un espace
